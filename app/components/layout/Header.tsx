7f18e485cf7260ad1bfa23518abd99192401767c
zcdev <zcdev@hotmail.com>
2026-02-20 17:26:09 -0800

    style(layout): implement header and global styles

---
+++ app/components/layout/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="py-16">
+            <h1 className="font-headline text-6xl text-amber-400">Food Truck Tracker</h1>
+            <h2 className="text-2xl text-yellow-100 tracking-normal">Your next bite is on the <em className='tracking-widest text-flame'>move</em>.</h2>
+        </header>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Header from './components/layout/Header';
+import FoodTruckPage from './food-trucks/page';
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center dark:bg-black">
-      <main className="flex w-full max-w-6xl flex-col items-center justify-between py-16 px-16 bg-white dark:bg-black sm:items-start border">
-        <h1 className="font-headline text-6xl">Food Truck Tracker</h1>
-        <p className="text-2xl">Hi, you're the best thing ever since sliced bread.</p>
+    <div className="flex items-center justify-center bg-stone-800">
+      <main className="flex w-full max-w-5xl flex-col items-center justify-center pb-16 px-16 sm:items-start">
+        <Header />
+        <FoodTruckPage />
       </main>
     </div>
   );
