c4fd92c34c2ababa027cb6bc8993fe35d7ece2ff
zcdev <zcdev@hotmail.com>
2026-08-07 15:19:25 -0700

    fix(a11y): adjust reader experience to skip non-essential elements

--- app/components/layout/Nav.tsx
+++ app/components/layout/Nav.tsx
@@ -15,11 +15,11 @@ export default function Nav() {
 
     return (
         <nav className="flex flex-col flex-row gap-0 md:gap-6 justify-center md:justify-start text-sm md:text-lg font-medium text-white text-center md:text-left mb-4 md:mb-0 mt-3">
-            <Link href={`/?${params}#hot-menu`} className="hover:text-orange-600 transition-colors ">Hot Menu</Link>
-            <span className="px-4 md:px-0"> | </span>
-            <Link href={`/?${params}#schedule`} className="hover:text-orange-600 transition-colors ">Schedule</Link>
-            <span className="px-4 md:px-0"> | </span>
-            <Link href={`/?${params}#about`} className="hover:text-orange-600 transition-colors ">About Us</Link>
+            <Link href={`/?${params}#hot-menu`} className="hover:text-orange-600 transition-colors">Hot Menu</Link>
+            <span className="px-4 md:px-0" aria-hidden="true"> | </span>
+            <Link href={`/?${params}#schedule`} className="hover:text-orange-600 transition-colors">Schedule</Link>
+            <span className="px-4 md:px-0" aria-hidden="true"> | </span>
+            <Link href={`/?${params}#about`} className="hover:text-orange-600 transition-colors">About Us</Link>
         </nav>
     );
 }
